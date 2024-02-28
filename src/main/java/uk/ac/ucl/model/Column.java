ae07eaeada2614142545046dac89b97770f90d36
nblandos <nblandos@gmail.com>
2024-02-28 12:46:23 +0000

    add column boilerplate to model

---
+++ src/main/java/uk/ac/ucl/model/Column.java
@@ -0,0 +1,28 @@
+package uk.ac.ucl.model;
+
+import java.util.ArrayList;
+
+public class Column {
+    public void addColumn(){
+
+    }
+    public ArrayList<String> getColumnNames() {
+
+    }
+
+    public int getRowCount() {
+
+    }
+
+    public int getValue(String columnName, int row) {
+
+    }
+
+    public void putValue(String columnName, int row, String value) {
+
+    }
+
+    public void addValue(String columnName, String Value) {
+
+    }
+}
