5e8d677a7dcde47171817fd2c0d6f887c0b3f0a3
Nicholas Blandos <nblandos@gmail.com>
2024-03-02 17:41:16 +0000

    Add Model and DataLoader and adapt ModelFactory

---
+++ src/main/java/uk/ac/ucl/model/DataLoader.java
@@ -0,0 +1,43 @@
+package uk.ac.ucl.model;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class DataLoader {
+
+    private String filePath;
+
+    public DataLoader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public DataFrame loadData() {
+        DataFrame dataFrame = new DataFrame();
+
+        try (Reader reader = new FileReader(filePath);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT))
+        {
+            String[] columnNames = csvParser.getHeaderMap().keySet().toArray(new String[0]);
+            for (String columnName : columnNames) {
+                dataFrame.addColumn(columnName);
+            }
+
+            for (CSVRecord csvRecord : csvParser)
+            {
+                for (String columnName : columnNames) {
+                    dataFrame.addValue(columnName, csvRecord.get(columnName));
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return dataFrame;
+    }
+}
--- src/main/java/uk/ac/ucl/model/Model.java
+++ src/main/java/uk/ac/ucl/model/Model.java
@@ -11,40 +11,32 @@ import org.apache.commons.csv.CSVRecord;
 
 public class Model
 {
-  // The example code in this class should be replaced by your Model class code.
-  // The data should be stored in a suitable data structure.
+  private DataFrame dataFrame;
+
+  public Model() {
+    this.dataFrame = new DataFrame();
+  }
+
+  public DataFrame getDataFrame() {
+    return dataFrame;
+  }
+
+  public void loadDataFromCSV(String filePath) {
+    DataLoader dataLoader = new DataLoader(filePath);
+    this.dataFrame = dataLoader.loadData();
+  }
 
   public List<String> getPatientNames()
   {
-    return readFile("data/patients100.csv");
+    //TODO
+    return dataFrame.getColumnNames();
+//    return dataFrame.getColumnValues("PatientName");
   }
 
-  // This method illustrates how to read csv data from a file.
-  // The data files are stored in the root directory of the project (the directory your project is in),
-  // in the directory named data.
-  public List<String> readFile(String fileName)
-  {
-    List<String> data = new ArrayList<>();
-
-    try (Reader reader = new FileReader(fileName);
-         CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT))
-    {
-      for (CSVRecord csvRecord : csvParser)
-      {
-        // The first row of the file contains the column headers, so is not actual data.
-        data.add(csvRecord.get(0));
-      }
-    } catch (IOException e)
-    {
-      e.printStackTrace();
-    }
-    return data;
-  }
-
-  // This also returns dummy data. The real version should use the keyword parameter to search
-  // the data and return a list of matching items.
   public List<String> searchFor(String keyword)
   {
-    return List.of("Search keyword is: "+ keyword, "result1", "result2", "result3");
+    //TODO
+    // add method to dataFrame to get list from keyword
+    return dataFrame.searchByColumnValue("PatientName", keyword);
   }
 }
